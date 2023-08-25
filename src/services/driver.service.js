85b1e115e5611cfcba35cc14e671980857eed3d0
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-08-25 14:14:12 +0700

    up

---
+++ src/services/driver.service.js
@@ -0,0 +1,54 @@
+const db = require("../models");
+const errorCode = require("../exeption_code/index");
+
+module.exports.createdriverService = (body) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const found = await db.driver.count({
+        where: { driverPhoneNumber: body.driverPhoneNumber },
+      });
+
+      if (found)
+        return reject({
+          status: errorCode.carBrandName_has_been_used,
+          message: "driverPhoneNumber has been used!",
+        });
+      const driver = await db.driver.create({
+        driverName: body.driverName,
+        driverPhoneNumber: body.driverPhoneNumber,
+      });
+
+      resolve(driver);
+    } catch (err) {
+      reject({ message: err.message });
+    }
+  });
+};
+module.exports.getAlldriverService = ({ ...query }) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const queries = { raw: true, rest: true };
+      const response = await db.driver.findAll({
+        where: query,
+        ...queries,
+      });
+      resolve(response);
+    } catch (err) {
+      reject({ message: err.message });
+    }
+  });
+};
+module.exports.getOneDriverService = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const queries = { raw: true, rest: true };
+      queries.id = id;
+      const response = await db.driver.findOne({
+        where: { driverUuid: id },
+      });
+      resolve(response);
+    } catch (err) {
+      reject({ message: err.message });
+    }
+  });
+};
