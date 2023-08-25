576265a03f2d7ec94d273e2807cc3dc1cad3911e
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-08-25 11:42:26 +0700

    up

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   registerUserController,
   registerMutipleUserController,
+  getAllCarController,
 } = require("../controllers/user.controller");
 const router = express.Router();
 const db = require("../models");
@@ -25,56 +26,6 @@ router.get("/get-one-user-by-uuid/:id", async (req, res) => {
   res.json(user);
 });
 
-router.post("/create-a-car", async (req, res) => {
-  const {
-    currentLocationInHCM,
-    user_id,
-    car_brand_id,
-    car_model_id,
-    car_seri_id,
-    vehicle_type_id,
-    car_license_id,
-    car_id,
-    regis_id,
-  } = req.body;
-  try {
-    const car = await db.cars.create({
-      currentLocationInHCM,
-      user_id,
-      car_brand_id,
-      car_model_id,
-      car_seri_id,
-      vehicle_type_id,
-      car_license_id,
-    });
-
-    const regis_method = await db.car_register_method.create({
-      car_id,
-      regis_id,
-    });
-
-    return res.json({ car, regis_method });
-  } catch (err) {
-    return res.json(err);
-  }
-});
-
-router.get("/all", async (req, res) => {
-  const cars = await db.cars.findAll({
-    include: [
-      "car_brand",
-      "car_model",
-      "vehicle_type",
-      "license_plate_type",
-      "vehicle_type",
-      "car_seri",
-      "user",
-      "regis",
-    ],
-  });
-
-  // db.cars.add;
-  return res.json(cars);
-});
+// router.get("/all", getAllCarController);
 
 module.exports.userRoute = router;
